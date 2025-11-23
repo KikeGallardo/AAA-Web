0002be168290bba71c158e6c0de26b47608f90c7
sergioperiche1711 <sergioperiche1711@gmail.com>
2025-11-23 04:15:54 -0500

    Boton Torneos en nav bar

--- torneo.php
+++ torneo.php
@@ -19,6 +19,9 @@
                 <li><a href="programar.php"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" /></svg></a></li>
             </ul>
             <ul class="nav_links">
+                <li><a href="torneo.php"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21h7.5M12 17.25v3.75M15.75 4.5h3a.75.75 0 0 1 .75.75v2.25a4.5 4.5 0  0 1-4.5 4.5M8.25 4.5h-3a.75.75 0 0 0-.75.75v2.25a4.5 4.5 0 0 0 4.5 4.5m6.75-7.5a3.75 3.75 0 1 1-7.5 0m7.5 0h-7.5" /></svg></a></li>
+            </ul>
+            <ul class="nav_links">
                 <li><a href="arbitros.php"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" /></svg></a></li>
             </ul>
         </nav>
@@ -49,7 +52,5 @@
                 </tr>
             </tbody>
         </table>
-    
-    
 </body>
 </html>
